0559034a39f673747efef43e479145a0001f2a41
quasarswastik <quasarswastik>
2025-05-14 04:43:13 +0000

    Added new notebooks for testing git capability

---
+++ My Folder/Notebook Basics.py
@@ -0,0 +1,39 @@
+# Databricks notebook source
+print("Hello world!")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT "HELLO SQL";
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # tTOTLE
+# MAGIC OEKFPE
+# MAGIC ## TITELT3
+
+# COMMAND ----------
+
+AC = "HERGRE"
+
+# COMMAND ----------
+
+# MAGIC %run ./May2025
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %fs ls
+
+# COMMAND ----------
+
+dbutils.help()
+
+# COMMAND ----------
+
+display(dbutils.fs.ls('/databricks-datasets'))
+
+# COMMAND ----------
+
+
